337193c4f35d047fe537ea1eba6ddd3096b8ff3e
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2024-07-18 15:15:16 -0700

    draft slow clk sync functionality

--- firmware/inc/config.h
+++ firmware/inc/config.h
@@ -21,6 +21,8 @@
 #define SLOW_SYNC_UART (uart0)
 #define SLOW_SYNC_BAUDRATE (1000UL)
 #define SLOW_SYNC_CLKOUT_PIN (0)
+#define SLOW_SYNC_START_OFFSET_US (0) // Offset from spec to account for
+                                      // transmission time.
 
 #define LED_PIN (25)
 
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -22,28 +22,43 @@ const uint8_t fw_version_major = 0;
 const uint8_t fw_version_minor = 0;
 const uint16_t serial_number = 0;
 
+// Note: literals, arrays, and functions accessed via interrupt are placed in
+//  RAM to avoid needing to access Flash.
+
 
 // Alarm number used to trigger dispatch of periodic time msg.
-int32_t harp_clkout_alarm_num = -1;
-uint32_t harp_clkout_irq_number;
+int32_t __not_in_flash("double_buffers") harp_clkout_alarm_num = -1;
+uint32_t __not_in_flash("double_buffers") harp_clkout_irq_number;
 
-int32_t slow_clkout_alarm_num = -1;
-uint32_t slow_clkout_irq_number;
+volatile uint __not_in_flash("double_buffers") harp_clkout_dma_chan;
+volatile uint __not_in_flash("double_buffers") slow_clkout_dma_chan;
 
-volatile uint harp_clkout_dma_chan;
-volatile uint slow_clkout_dma_chan;
+// Ping-Pong Buffer for outgoing msg buffer for the CLKOUT msg.
+// Note: "0xAA 0xAF" prefix is built-in and should not be altered. Data
+//  starts at harp_time_msg_x[2].
+volatile uint8_t __not_in_flash("double_buffers")harp_time_msg_a[6] =
+    {0xAA, 0xAF, 0, 0, 0, 0};
+volatile uint8_t __not_in_flash("double_buffers")harp_time_msg_b[6] =
+    {0xAA, 0xAF, 0, 0, 0, 0};
 
-// Double-buffered outgoing msg buffer.
-volatile uint8_t harp_time_msg_a[6] = {0xAA, 0xAF, 0, 0, 0, 0};
-volatile uint8_t harp_time_msg_b[6] = {0xAA, 0xAF, 0, 0, 0, 0};
+// Pointers for Ping-Pong buffer.
+volatile uint8_t __not_in_flash("double_buffers") *dispatch_buffer;
+volatile uint8_t __not_in_flash("double_buffers") *load_buffer;
 
-volatile uint8_t *dispatch_buffer;
-volatile uint8_t *load_buffer;
 
-//inline void disable_led() {gpio_put(LED_PIN, 0);}
+int32_t __not_in_flash("double_buffers") slow_clkout_alarm_num = -1;
+uint32_t __not_in_flash("double_buffers") slow_clkout_irq_number;
+
+// Ping-Pong Buffer
+volatile uint32_t __not_in_flash("double_buffers") slow_clkout_seconds_a;
+volatile uint32_t __not_in_flash("double_buffers") slow_clkout_seconds_b;
+
+// Pointers for Ping-Pong buffer.
+volatile uint32_t __not_in_flash("double_buffers") *dispatch_second;
+volatile uint32_t __not_in_flash("double_buffers") *load_second;
 
 /**
- * \brief Dispatch the time to 16x outputs.
+ * \brief Dispatch the time to the 16x board outputs.
  * \warning called inside of interrupt. Does not block.
  */
 void __not_in_flash_func(dispatch_harp_clkout)()
@@ -52,8 +67,7 @@ void __not_in_flash_func(dispatch_harp_clkout)()
     dispatch_uart_stream(harp_clkout_dma_chan, HARP_UART, (uint8_t*)dispatch_buffer, 6);
     // Clear the latched hardware interrupt.
     timer_hw->intr |= (1u << harp_clkout_alarm_num);
-
-    // Calculate next whole harp time second. +2 bc we wake up *before* the
+    // Get the next whole harp time second. +2 bc we wake up *before* the
     // elapse of the next whole second when we are supposed to emit the msg.
     uint32_t curr_harp_seconds = HarpCore::harp_time_s();
     uint32_t next_msg_harp_time_us_32 = (curr_harp_seconds * 1000000UL) + 2'000'000UL;
@@ -64,11 +78,10 @@ void __not_in_flash_func(dispatch_harp_clkout)()
 #endif
     // Offset such that the start of last byte occurs on the whole second per:
     // https://harp-tech.org/protocol/SynchronizationClock.html#serial-configuration
-    // Offset such that the start of last byte occurs on the whole second per:
-    // https://harp-tech.org/protocol/SynchronizationClock.html#serial-configuration
     next_msg_harp_time_us_32 += HARP_SYNC_START_OFFSET_US;
     // Update time contents in the next message.
-    memcpy((void*)(load_buffer + 2), (void*)(&curr_harp_seconds), 4);
+    memcpy((void*)(load_buffer + 2), (void*)(&curr_harp_seconds),
+           sizeof(curr_harp_seconds));
     // Toggle ping-pong buffers.
     std::swap(load_buffer, dispatch_buffer);
     gpio_put(LED_PIN, !gpio_get(LED_PIN)); // toggle LED.
@@ -81,10 +94,37 @@ void __not_in_flash_func(dispatch_harp_clkout)()
     timer_hw->alarm[harp_clkout_alarm_num] = alarm_time_us;
 }
 
-void __not_in_flash_func(dispatch_slow_sync_clkout)()
+/**
+ * \brief Dispatch the time on the auxilary output.
+ */
+void __not_in_flash_func(dispatch_slow_clkout)()
 {
-    // TODO: implement this.
-//    dispatch_uart_stream(SLOW_SYNC_UART, (uint8_t*)&dispatch_buffer, 4);
+    // Dispatch the previously-configured time.
+    dispatch_uart_stream(slow_clkout_dma_chan, SLOW_SYNC_UART,
+                         (uint8_t*)dispatch_second, sizeof(dispatch_second));
+    // Clear the latched hardware interrupt.
+    timer_hw->intr |= (1u << slow_clkout_alarm_num);
+    // Get the next whole harp time second.
+    uint32_t curr_harp_seconds = HarpCore::harp_time_s();
+    uint32_t next_msg_harp_time_us_32 = (curr_harp_seconds * 1000000UL) + 1'000'000UL;
+    // Account for extra second if we wake up *before* the elapse of the next
+    // whole second.
+    if (SLOW_SYNC_START_OFFSET_US < 0)
+        next_msg_harp_time_us_32 += 1'000'000UL;
+    // Add offset (if any).
+    next_msg_harp_time_us_32 += SLOW_SYNC_START_OFFSET_US;
+    // Update time contents in the next message.
+    memcpy((void*)(load_second + 2), (void*)(&curr_harp_seconds),
+           sizeof(load_second));
+    // Toggle ping-pong buffers.
+    std::swap(load_second, dispatch_second);
+    // Schedule next time msg dispatch in system time.
+    // Low-level interface (fast!) to re-schedule this function.
+    uint32_t alarm_time_us = HarpCore::harp_to_system_us_32(next_msg_harp_time_us_32);
+    // Enable alarm to trigger interrupt.
+    timer_hw->inte |= (1u << slow_clkout_alarm_num);
+    // Arm alarm by writing the alarm time.
+    timer_hw->alarm[slow_clkout_alarm_num] = alarm_time_us;
 }
 
 // Create Harp App.
@@ -154,29 +194,35 @@ int main()
     timer_hw->inte |= (1u << harp_clkout_alarm_num);
     // Arm alarm by writing the alarm time.
     timer_hw->alarm[harp_clkout_alarm_num] = alarm_time_us;
-#if defined(DEBUG)
+#if defined(DEBUG) // We can't use the DEBUG port and AuxPort at the same time.
     printf("Curr Harp seconds: %lu[s] | 1st alarm (Harp) time %lu[us] | 1st alarm (sys time) %lu[us].\r\n",
            curr_harp_seconds, next_msg_harp_time_us_32, alarm_time_us);
     printf("Scheduled first alarm.\r\n");
 #else
-/*
-// Setup SLOW CLKOUT periodic outgoing time message.
-// Leverage RP2040 ALARMs to dispatch periodically via interrupt handler.
+    // Setup SLOW CLKOUT periodic outgoing time message.
+    // Setup Outgoing msg double buffer;
+    dispatch_second = &slow_clkout_seconds_a;
+    load_second = &slow_clkout_seconds_b;
+    // Setup SLOW CLKOUT periodic outgoing time message.
+    // Leverage RP2040 ALARMs to dispatch periodically via interrupt handler.
     slow_clkout_alarm_num = hardware_alarm_claim_unused(true); // true --> required
-    slow_clkout_irq_number = TIMER_IRQ_0 + clkout_alarm_num; // harware_alarm_irq_number() in sdk.
+    slow_clkout_irq_number = TIMER_IRQ_0 + slow_clkout_alarm_num; // harware_alarm_irq_number() in sdk.
     // Attach interrupt to function and eneable alarm to generate interrupt.
-    irq_set_exclusive_handler(slow_clkout_irq_number, dispatch_slow_sync_clkout);
+    irq_set_exclusive_handler(slow_clkout_irq_number, dispatch_slow_clkout);
     irq_set_enabled(slow_clkout_irq_number, true);
-    // Compute start time for slow CLKOUT msg (next whole second).
-    uint32_t next_slow_msg_harp_time_us_32 = (curr_harp_seconds * 1000000UL) + 1'000'000UL;
+    // Compute start time for Slow CLKOUT msg.
+    // Get the next whole second.
+    curr_harp_seconds = HarpCore::harp_time_s();
+    next_msg_harp_time_us_32 = (curr_harp_seconds * 1000000UL) + 1'000'000UL;
+    // Apply offset if any.
+    next_msg_harp_time_us_32 += SLOW_SYNC_START_OFFSET_US;
     // Schedule next time msg dispatch in system time.
     // Low-level interface (fast!) to re-schedule this function.
-    alarm_time_us = HarpCore::harp_to_system_us_32(next_slow_msg_harp_time_us_32);
+    alarm_time_us = HarpCore::harp_to_system_us_32(next_msg_harp_time_us_32);
     // Enable alarm to trigger interrupt.
     timer_hw->inte |= (1u << slow_clkout_alarm_num);
     // Arm alarm by writing the alarm time.
     timer_hw->alarm[slow_clkout_alarm_num] = alarm_time_us;
-*/
 #endif
     while(true)
         app.run();
